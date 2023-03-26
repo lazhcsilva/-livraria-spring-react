da9a73e15bf341390ba7888107b409db1fed6423
Lázaro Henrique Silva <lazhcsilva@gmail.com>
2023-03-26 02:54:39 -0300

    Additing a create method service

--- livraria-spring/src/main/java/br/com/livraria/service/BookService.java
+++ livraria-spring/src/main/java/br/com/livraria/service/BookService.java
@@ -1,18 +1,40 @@
 package br.com.livraria.service;
 
 import br.com.livraria.model.BookModel;
+import br.com.livraria.model.ResponseModel;
 import br.com.livraria.repository.BookRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
     @Autowired
-    private BookRepository br;
+    private BookRepository bookRepository;
+
+    @Autowired
+    private ResponseModel responseModel;
 
     public Iterable<BookModel> list(){
-        return br.findAll();
+        return bookRepository.findAll();
+    }
+
+    public ResponseEntity<?> register(BookModel bookModel){
+        if(bookModel.getName().equals("")){
+            responseModel.setMessage("O nome do livro é obrigatório!");
+            return new ResponseEntity<ResponseModel>(responseModel, HttpStatus.BAD_REQUEST);
+        } else if (bookModel.getAuthor().equals("")){
+            responseModel.setMessage("O nome do autor é obrigatório!");
+            return new ResponseEntity<ResponseModel>(responseModel, HttpStatus.BAD_REQUEST);
+        } else if (bookModel.getPages().equals("")){
+            responseModel.setMessage("O número de páginas é obrigatório!");
+            return new ResponseEntity<ResponseModel>(responseModel, HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<BookModel>(bookRepository.save(bookModel), HttpStatus.CREATED);
+        }
     }
 
 }
